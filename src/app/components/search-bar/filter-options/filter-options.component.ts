1a6bc542f09e0d0a4e0490171e3198da2c50bed3
dzixper <dzumper@gmail.com>
2020-08-16 18:44:34 +0200

    close dialog functionality in filter options

--- src/app/components/search-bar/filter-options/filter-options.component.ts
+++ src/app/components/search-bar/filter-options/filter-options.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-filter-options',
   templateUrl: './filter-options.component.html',
-  styleUrls: ['./filter-options.component.scss']
+  styleUrls: ['./filter-options.component.scss'],
 })
 export class FilterOptionsComponent implements OnInit {
+  levels: Array<string> = ['All', 'Junior', 'Mid', 'Senior'];
 
-  levels: Array<string> = [
-    'All',
-    'Junior',
-    'Mid',
-    'Senior'
-  ];
+  constructor(private dialogRef: MatDialog) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  closeDialog(): void {
+    this.dialogRef.closeAll();
   }
 
+  ngOnInit(): void {}
 }
