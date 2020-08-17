814b19feef4020b70fb80461b674ed206f6fc2c5
dzixper <dzumper@gmail.com>
2020-08-17 12:51:03 +0200

    Post a job component styled

---
+++ src/app/components/post-offer/post-offer-form/post-offer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-offer-form',
+  templateUrl: './post-offer-form.component.html',
+  styleUrls: ['./post-offer-form.component.scss']
+})
+export class PostOfferFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
