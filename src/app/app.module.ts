2cc395552e39c7dbc78e2514dfe2d86b44a43323
dzixper <dzumper@gmail.com>
2020-08-17 12:50:52 +0200

    Post a job component styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { PostOfferComponent } from './components/post-offer/post-offer.component
 import { MainOfferPageComponent } from './components/main-offer-page/main-offer-page.component';
 import { OffersContentComponent } from './components/offers/offers-content/offers-content.component';
 import { PricingComponent } from './components/post-offer/pricing/pricing.component';
+import { PostOfferFormComponent } from './components/post-offer/post-offer-form/post-offer-form.component';
 
 const appRoutes: Routes = [
   { path: '', component: MainOfferPageComponent },
   { path: 'post-offer', component: PostOfferComponent },
+  { path: 'post-offer-form', component: PostOfferFormComponent }
   ];
 
 @NgModule({
@@ -32,6 +34,7 @@ const appRoutes: Routes = [
     MainOfferPageComponent,
     OffersContentComponent,
     PricingComponent,
+    PostOfferFormComponent,
   ],
   imports: [
     BrowserModule,
